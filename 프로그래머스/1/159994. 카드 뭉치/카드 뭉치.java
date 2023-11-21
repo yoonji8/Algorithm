29059085d617a58edbd72241c4b54ddf082745c9
YunJi Nam <135217229+yunji-nam@users.noreply.github.com>
2023-11-22 00:14:22 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 90.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        int j=0;
+        int k=0;
+        
+        for(int i=0;i<goal.length;i++){
+            if(j < cards1.length && cards1[j].equals(goal[i])) {
+                j++;
+            } else if(k < cards2.length && cards2[k].equals(goal[i])) {
+                k++;
+            } else {
+                return "No";
+            }
+        }
+        return "Yes";
+    }
+}
