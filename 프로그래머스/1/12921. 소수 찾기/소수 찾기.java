fc6d8ddc6594ffd250e87e593bff4187a33b7d20
YunJi Nam <135217229+yunji-nam@users.noreply.github.com>
2023-11-30 15:18:08 +0900

    [level 1] Title: 소수 찾기, Time: 349.47 ms, Memory: 70.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i=2; i<=n; i++) {
+            boolean check = true;
+            for(int j=2; j<=Math.sqrt(i); j++) {
+                if(i%j == 0) {
+                    check = false;
+                    break;
+                }
+                
+            }
+            if(check == true) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
