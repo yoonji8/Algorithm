dbd36bc22012c2937df3a0a935f85a2235203647
YunJi Nam <135217229+yunji-nam@users.noreply.github.com>
2024-01-11 23:54:49 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 7.37 ms, Memory: 85.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[2];
+            
+        while(!s.equals("1")) {
+            int cnt = 0;
+            for(int i=0; i<s.length(); i++) {
+                if(s.charAt(i) == '0') {
+                    answer[1]++;
+                }else {
+                    cnt++;
+                }
+            }
+            s = Integer.toBinaryString(cnt);
+            answer[0]++;
+        }
+        
+        return answer;
+    }
+}
