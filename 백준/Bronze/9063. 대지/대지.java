a7ce2538e78223fa690ddd6b89a405258774d30d
YunJi Nam <135217229+yunji-nam@users.noreply.github.com>
2024-10-05 23:56:52 +0900

    [Bronze III] Title: 대지, Time: 312 ms, Memory: 31412 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] x = new int[N];
+        int[] y = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            x[i] = Integer.parseInt(st.nextToken());
+            y[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(x);
+        Arrays.sort(y);
+
+        System.out.println((x[N-1]-x[0])*(y[N-1]-y[0]));
+    }
+}
